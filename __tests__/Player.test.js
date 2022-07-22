06b5129ff037b6e58bee99e40810b8202d6672a6
Ryan Dooley <Rdoolz51@aol.com>
2022-07-22 06:29:08 -0400

    added mock test case for potion in the player class

---
+++ __tests__/Player.test.js
@@ -0,0 +1,17 @@
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+test('creates a player object', () => {
+    const player = new Player('Ryan');
+
+    expect(player.name).toBe('Ryan');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
